a9e51b6868328b0bd481a5b0f5a0911cd4969181
Y0nyx <72567319+Y0nyx@users.noreply.github.com>
2021-06-03 13:40:10 -0400

    html

---
+++ mathiaslegrand/exemple-1/src/main/webapp/design.js
@@ -0,0 +1,135 @@
+
+webix.ready(function(){
+	if (webix.CustomScroll)
+		webix.CustomScroll.init();
+	webix.ui({
+	"rows": [
+		{
+			"autoheight": false,
+			"view": "form",
+			"rows": [
+				{
+					"view": "text",
+					"label": "Nom",
+					"name": "name"
+				},
+				{
+					"view": "text",
+					"label": "Prenom",
+					"prenom": "prenom"
+				},
+				{
+				
+					"view": "text",
+					"label": "CIP",
+					"cip": "cip"
+				},
+				{
+					"view": "button",
+					"css": "webix_primary",
+					"label": "Save"
+				},
+				{
+					
+					"columns": [
+						{
+							"id": "title",
+							"header": "Title",
+							"fillspace": true,
+							"sort": "string"
+						},
+						{
+							"id": "year",
+							"header": "Year",
+							"sort": "string"
+						},
+						{
+							"id": "votes",
+							"header": "Votes",
+							"sort": "string"
+						},
+						{
+							"id": "rating",
+							"header": "Rating",
+							"sort": "string"
+						},
+						{
+							"id": "rank",
+							"header": "Rank",
+							"sort": "string"
+						},
+						{
+							"id": "category",
+							"header": "Category",
+							"sort": "string"
+						}
+					],
+					"view": "datatable",
+					"height": 79
+				},
+				{
+					"autoheight": false,
+					"view": "form",
+					"rows": [
+						{
+							"view": "text",
+							"label": "User name",
+							"name": "name"
+						},
+						{
+							"view": "text",
+							"label": "Email",
+							"name": "email"
+						},
+						{
+							"view": "button",
+							"css": "webix_primary",
+							"label": "Save",
+							"height": 106
+						},
+						{
+							"url": "demo->60b90882fd710000187ea330",
+							"columns": [
+								{
+									"id": "title",
+									"header": "Data table des clients",
+									"fillspace": true,
+									"sort": "string",
+									"hidden": false
+								},
+								{
+									"id": "year",
+									"sort": "string",
+									"fillspace": false,
+									"hidden": false
+								},
+								{
+									"id": "votes",
+									"header": "Votes",
+									"sort": "string"
+								},
+								{
+									"id": "rating",
+									"header": "Rating",
+									"sort": "string"
+								},
+								{
+									"id": "rank",
+									"header": "Rank",
+									"sort": "string"
+								},
+								{
+									"id": "category",
+									"header": "Category",
+									"sort": "string"
+								}
+							],
+							"view": "datatable"
+						}
+					]
+				}
+			]
+		}
+	]
+});
+});
