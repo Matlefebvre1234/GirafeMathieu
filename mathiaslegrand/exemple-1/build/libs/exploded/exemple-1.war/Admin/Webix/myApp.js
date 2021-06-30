39e42639dfc43f2b9fc3fa342ac0292861adb2f5
Mathieu <mat_lefebvre12@hotmail.com>
2021-06-30 15:26:17 -0400

    FUCKING CHROME

--- mathiaslegrand/exemple-1/build/libs/exploded/exemple-1.war/Admin/Webix/myApp.js
+++ mathiaslegrand/exemple-1/build/libs/exploded/exemple-1.war/Admin/Webix/myApp.js
@@ -26,7 +26,7 @@ webix.ready(function() {
                             {
                             view: "multiview",
                                 cells: [
-                                    { id: "Inventaire", "rows": [Inventaire] },
+                                    { id: "Inventaire", "rows": [Inventaire_buttons,Inventaire] },
                                     { id: "Precommande", "rows": [Precommande] },
                                     { id: "Commande", "rows": [Commande] },
                                     { id: "Log", "rows": [log] }
